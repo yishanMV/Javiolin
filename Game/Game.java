f1c845eecc46298bbc4d8b361fc83548a8c71178
yishanMV <ylin101@student.fuhsd.org>
2025-04-25 11:16:53 -0700

    Add files via upload

--- Game/Game.java
+++ Game/Game.java
@@ -1,37 +1,42 @@
+//Yishan Lin and Stanley Wang
+//Javiolin
+//Code v1, home page, game page, card layout demonstration
+
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.ImageIcon;
-import javax.swing.Icon;
-import java.awt.Color;
-
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
+import javax.swing.Timer;
 
 
 public class Game
 {
 	private int accuracyPercent;
 	private int notesCountTotal;
+    private int gameDuration;
 	private boolean oneRoundOver;
 	private Image[] images;
+    private File[] audioFiles;
+    private GamePanelHolder gph;
 
     private CardLayout cl;
 
     public Game()
     {
-        cl = new CardLayout(); // Initialize cl
+        cl = new CardLayout();
         getMyImage();
+        getMyAudioFiles();
+
     }
 
     public static void main(String[] args)
@@ -48,7 +53,7 @@ public class Game
         frame.setLocation(100,100);
         frame.setResizable(false);
 
-        GamePanelHolder gph = new GamePanelHolder();
+        gph = new GamePanelHolder();
         frame.getContentPane().add(gph);
         frame.setVisible(true);
         
@@ -59,7 +64,7 @@ public class Game
         public GamePanelHolder()
         {
 			
-            setLayout(cl); // cl is now initialized
+            setLayout(cl);
 
             HomePanel hp = new HomePanel();
             GamePanel gp = new GamePanel();
@@ -73,14 +78,19 @@ public class Game
             add(sp, "settings");
 
             cl.show(this, "home");
-
-
         }
+
+        public void showCard(String cardName)
+        {
+            cl.show(this, cardName);
+        }
+
+
     }
     
    	public void getMyImage() 
 	{
-		String[] imageName = new String[]{"settings.png"};
+		String[] imageName = new String[]{"settings.png", "background1.png", "title.png", "paper.png", "selection.png", "loading.png", "homebutton.png"};
 		images = new Image[imageName.length];
 		for(int i = 0; i < imageName.length; i++)
 		{
@@ -90,7 +100,7 @@ public class Game
 			}
 			catch(IOException e)
 			{
-				System.err.println("\n\n\n" + imageName + " can't be found.\n\n");
+				System.err.println("\n\n\n" + imageName[i] + " can't be found.\n\n");
 				e.printStackTrace();
 			}
 				
@@ -98,27 +108,62 @@ public class Game
 		}
 	}
 
+    public void getMyAudioFiles()
+    {
+        String[] audioNames = new String[]{};
+        audioFiles = new File[audioNames.length];
+        for(int i = 0; i < audioNames.length; i++)
+        {
+            try
+            {
+                audioFiles[i] = new File("Audio/" + audioNames[i]);
+            }
+            catch(Exception e)
+            {
+                System.err.println("\n\n\n" + audioNames[i] + " can't be found.\n\n");
+                e.printStackTrace();
+            }
+        }
+    }
+
     class HomePanel extends JPanel
     {
         Font title;
         Font buttonText;
         Font subtitle;
-        Icon settingsIcon;
 
         public HomePanel()
         {
 			
 			Color WHITE = new Color(255, 255, 255);
 			setBackground(WHITE);
-			settingsIcon = new ImageIcon(images[0]);
             title = new Font("Arial", Font.BOLD, 100);
             buttonText = new Font("Arial", Font.PLAIN, 50);
             subtitle = new Font("Arial", Font.PLAIN, 20);
 
             setLayout(null);
-            JButton play = new JButton("Play"); //note im playing on making ImageIcon, do some research!!
-            JButton help = new JButton("Help");
-            JButton settings = new JButton(settingsIcon);
+            JButton play = new JButton(""); 
+            play.setContentAreaFilled(false); 
+            play.setBorderPainted(false); 
+            play.setFocusPainted(true); 
+            play.setFont(buttonText);
+            play.setActionCommand("Play");
+
+
+            JButton help = new JButton("");
+            help.setContentAreaFilled(false);
+            help.setBorderPainted(false);
+            help.setFocusPainted(true);
+            help.setFont(buttonText);
+            help.setActionCommand("Help");
+
+
+            JButton settings = new JButton("");
+            settings.setContentAreaFilled(false);
+            settings.setBorderPainted(false);
+            settings.setFocusPainted(true);
+            settings.setFont(buttonText);
+            settings.setActionCommand("Settings");
           
             
 
@@ -141,13 +186,14 @@ public class Game
 
             JButton settingsIcon = new JButton("Settings");
 
-            play.setBounds(100, 250, 200, 100);
-            help.setBounds(100, 400, 200, 100);
+            play.setBounds(520, 308, 280, 90);
+            help.setBounds(520, 406, 280, 90);
             
-            accuracy.setBounds(700, 300, 200, 30);
-            notesPlayed.setBounds(700, 350, 200, 30);
-            highscore.setBounds(700, 400, 200, 30);
-            settings.setBounds(700, 75, 100, 100);
+            accuracy.setBounds(275, 350, 200, 30);
+            notesPlayed.setBounds(275, 375, 200, 30);
+            highscore.setBounds(275, 400, 200, 30);
+
+            settings.setBounds(520, 503, 280, 86);
 
             add(play);
             add(help);
@@ -163,15 +209,16 @@ public class Game
 					String command = evt.getActionCommand();
 					if(command.equals("Play")) 
 					{
-						cl.show(
+						gph.showCard("game");
 					}
 					else if(command.equals("Help"))
 					{
+                        gph.showCard("instructions");
 
 					}
-					else if(command.equals(settingsIcon))
+					else if(command.equals("Settings"))
 					{
-						
+						gph.showCard("settings");
 					}
 				}
 			}
@@ -187,11 +234,11 @@ public class Game
         public void paintComponent(Graphics g)
         {
             super.paintComponent(g);
-            g.setFont(title);
-            g.drawString("Javiolin", 300, 150);
+            g.drawImage(images[1], 0, 0, getWidth(), getHeight(), this);
+            g.drawImage(images[2], 300, 50, 400, 100, this);
+            g.drawImage(images[4], 225, 250, 600, 400, this); 
             g.setFont(subtitle);
-            g.drawString("My Stats", 700, 250);
-            //g.drawImage(images[0], 300, 300, 100, 100, this);
+            g.drawString("My Stats", 275, 325);
         }
     }
 
@@ -201,9 +248,32 @@ public class Game
 
         public InstructionPanelHolder()
         {
-            infoPages = new CardLayout(); // Initialize infoPages
-            setLayout(infoPages); // infoPages is now initialized
+            infoPages = new CardLayout();
+            setLayout(infoPages);
         }
+
+
+        class firstInstructionPanel extends JPanel
+        {
+            public firstInstructionPanel()
+            {
+                setLayout(null);
+            }
+        }
+        class secondInstructionPanel extends JPanel
+        {
+            public secondInstructionPanel()
+            {
+                setLayout(null);
+            }
+        }
+
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            g.drawImage(images[1], 0, 0, getWidth(), getHeight(), this);
+        }
+
 
 
     }
@@ -214,18 +284,49 @@ public class Game
 
         public GamePanel()
         {
-            gamePages = new CardLayout(); // Initialize gamePages
-            setLayout(gamePages); // gamePages is now initialized
+            gamePages = new CardLayout();
+            setLayout(gamePages);
+            loadingPanel loading = new loadingPanel();
             mainGamePanel mainGame = new mainGamePanel();
             resultsPanel results = new resultsPanel();
 
+            add(loading, "loading");
             add(mainGame, "main game");
             add(results, "results");
-            
 
-            gamePages.show(this, "main game");
-            
+            gamePages.show(this, "loading"); 
+        }
 
+        public void changeScene(String scene)
+        {
+            gamePages.show(this, scene);
+        }
+
+        class loadingPanel extends JPanel
+        {
+            private Timer timer;
+
+            public loadingPanel()
+            {
+                setBackground(new Color(30, 30, 30));
+                startTransition();
+            }
+
+            private void startTransition()
+            {
+                timer = new Timer(3000, new LoadingTransitionHandler());
+                timer.setRepeats(false); 
+                timer.start();
+            }
+
+            class LoadingTransitionHandler implements ActionListener
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    timer.stop();
+                    changeScene("main game");
+                }
+            }
         }
 
         class mainGamePanel extends JPanel
@@ -233,6 +334,12 @@ public class Game
             public mainGamePanel()
             {
                 setLayout(null);
+            }
+
+            public void paintComponent(Graphics g)
+            {
+                super.paintComponent(g);
+                g.drawImage(images[1], 0, 0, getWidth(), getHeight(), this);
             }
         }
 
@@ -243,6 +350,12 @@ public class Game
                 setLayout(null);
             }
         }
+
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            g.drawImage(images[1], 0, 0, getWidth(), getHeight(), this);
+        }
     }
 
     class SettingsPanel extends JPanel
@@ -250,6 +363,12 @@ public class Game
         public SettingsPanel()
         {
             setLayout(null);
+        }
+
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            g.drawImage(images[1], 0, 0, getWidth(), getHeight(), this);
         }
     }
 
